5cccb2d0e327cbb76ce3fd31b2ef10b72ad0dc40
Alex Ferreira <alexferreiracp@gmail.com>
2015-10-06 21:42:28 -0300

    Iniciando jogador Humano

---
+++ src/com/github/awvalenti/cruzes/implementacao/JogadorHumanoScanner.java
@@ -0,0 +1,20 @@
+package com.github.awvalenti.cruzes.implementacao;
+
+import java.util.Scanner;
+
+import com.github.awvalenti.cruzes.api.interfaces.Jogador;
+import com.github.awvalenti.cruzes.api.interfaces.Movimento;
+
+public class JogadorHumanoScanner implements Jogador{
+
+	@Override
+	public Movimento obterMovimentoDesejado() {
+		
+		Scanner ler= new Scanner(System.in);
+		
+		
+		
+		return null;
+	}
+
+}
