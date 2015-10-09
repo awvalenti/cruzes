07bbfce18cc1dbb6355a91b8fcd9bb80f8fa64e5
pinheiro000 <pinheiro000@hotmail.com>
2015-10-09 00:50:45 -0300

    Commit inicial

--- src/com/github/awvalenti/cruzes/implementacao/Match.java
+++ src/com/github/awvalenti/cruzes/implementacao/Match.java
@@ -2,8 +2,12 @@ package com.github.awvalenti.cruzes.implementacao;
 
 import com.github.awvalenti.cruzes.api.classesabstratas.Partida;
 import com.github.awvalenti.cruzes.api.interfaces.AnalisadorTabuleiro;
+import com.github.awvalenti.cruzes.api.interfaces.EstadoJogo;
 import com.github.awvalenti.cruzes.api.interfaces.FabricaTabuleiro;
 import com.github.awvalenti.cruzes.api.interfaces.Jogador;
+import com.github.awvalenti.cruzes.api.interfaces.Movimento;
+import com.github.awvalenti.cruzes.api.interfaces.TabuleiroLeitura;
+import com.github.awvalenti.cruzes.api.interfaces.TabuleiroLeituraEscrita;
 import com.github.awvalenti.cruzes.api.interfaces.VisualizacaoTabuleiro;
 
 public class Match extends Partida {
@@ -14,11 +18,21 @@ public class Match extends Partida {
 		super(jogador1, jogador2, fabrica, analisador, visualizacaoTabuleiro);
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	
+	
 	@Override
 	public void iniciar() {
 		// TODO Auto-generated method stub
-
+		/* jogador - Movimento obterMovimentoDesejado();
+		 * fabricatabuleiro - TabuleiroLeituraEscrita criarTabuleiro(int numeroLinhas, int numeroColunas)
+		 * analisadortabuleiro - EstadoJogo determinarEstado(TabuleiroLeitura tabuleiro)
+		 * visualizacaoTabuleiro void desenhar(TabuleiroLeitura tabuleiro);
+		 */
+		
+		Movimento movimentoj1 = jogador1.obterMovimentoDesejado();
+		
+	
 	}
 
 }
