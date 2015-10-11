73bbad7c764135c54b6921826ff5ed20ccd698b1
fabitous <fabitous@gmail.com>
2015-10-11 10:52:05 -0300

    Create Casa.java

---
+++ src/com/github/awvalenti/cruzes/implementacao/Casa.java
@@ -0,0 +1,15 @@
+package com.github.awvalenti.cruzes.implementacao;
+
+public class Casa {
+	
+	private Peca peca;
+
+	public Casa(Peca peca) {
+		super();
+		this.peca = peca;
+	}
+
+	public Peca getPeca() {
+		return peca;
+	}
+}
