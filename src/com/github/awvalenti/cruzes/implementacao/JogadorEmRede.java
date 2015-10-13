00f497c919d229126ed2157ec2be48c173171bbf
adncorinthiano <developer.adriano@gmail.com>
2015-10-13 18:25:38 -0300

    modificações finais na classe

--- src/com/github/awvalenti/cruzes/implementacao/JogadorEmRede.java
+++ src/com/github/awvalenti/cruzes/implementacao/JogadorEmRede.java
@@ -16,29 +16,34 @@ public class JogadorEmRede implements Jogador {
 	Socket    socketInput;
 	
 	public JogadorEmRede(int porta){
+		
 		try{
-		  ServerSocket socket = new ServerSocket(porta);
-		  this.socketInput = socket.accept();
+		
+		@SuppressWarnings("resource")
+		ServerSocket socket = new ServerSocket(porta);
+		this.socketInput = socket.accept();
 		}
+		
 		catch(IOException e){
 			e.printStackTrace();
 		}
 	}
 	
-	//DataInputStream
-	//DataOutputStream
+	
+	//passando um DataInputStream por linha/coluna porque não sei
+	//como passar o objeto completo, já que não tem um read especifico pra ele
+	
 	@Override
-	//passando um DataInputStream por linha/coluna porque nao sei
-	//como passar o objeto completo, ja que nao tem um read expecifico pra ele
-	//talvez trabalhar com bytes resolva isso mas nao sei
 	public Movimento obterMovimentoDesejado(DataInputStream linhaOrigem, DataInputStream colunaOrigem, 
 							DataInputStream linhaDestino, DataInputStream colunaDestino) throws IOException {
 		
 		//recebe os datainput como parametro, extrai o valor inteiro de cada um e instancia posicoes
+		
 		Posicao posicaoOrigem = new PosicaoConcreta(linhaOrigem.readInt(), colunaOrigem.readInt());
 		Posicao posicaoDestino = new PosicaoConcreta(linhaDestino.readInt(), colunaDestino.readInt());
 		
-		//constroi movimento a partir das posicoes extraidas
+		//constrói movimento a partir das posições extraídas
+		
 		movimentoDesejado = new MovimentoConcreto(posicaoOrigem, posicaoDestino);
 		
 		socketInput.close();
