77f7f5358af73574ef6a97b00863cb290385a9e2
fabitous <fabitous@gmail.com>
2015-11-10 19:57:40 -0200

    Update Main.java

--- src/com/github/awvalenti/cruzes/test/mock/Main.java
+++ src/com/github/awvalenti/cruzes/test/mock/Main.java
@@ -1,5 +1,6 @@
 package com.github.awvalenti.cruzes.test.mock;
 
+import com.github.awvalenti.cruzes.api.enums.ConteudoCasa;
 import com.github.awvalenti.cruzes.api.interfaces.EstadoJogo;
 import com.github.awvalenti.cruzes.implementacao.AnalisadorDoTabuleiro;
 
@@ -7,14 +8,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		TabuleiroLeituraImpl tab = new TabuleiroLeituraImpl();
-		tab.xisDuasPecasFechadas();
+		tab.inicioDoJogoTodasAsPecasNaPosicaoInicial();
 		AnalisadorDoTabuleiro analisador = new AnalisadorDoTabuleiro();
 		
 		EstadoJogo estado = analisador.determinarEstado(tab);
 		
 		System.out.println("Esta finalizado: "+estado.isFinalizado());
 		System.out.println("Vencedor: "+estado.getTimeVencedor());
-
+		
+		tab.imprimirTabuleiroEmChar();
 	}
-
 }
