c93a27e182117ee3249f0dbb0f423e049f8de7fe
fabitous <fabitous@gmail.com>
2015-11-10 20:04:48 -0200

    Update ConteudoCasa.java

--- src/com/github/awvalenti/cruzes/api/enums/ConteudoCasa.java
+++ src/com/github/awvalenti/cruzes/api/enums/ConteudoCasa.java
@@ -1,12 +1,9 @@
 package com.github.awvalenti.cruzes.api.enums;
 
-import com.github.awvalenti.cruzes.api.interfaces.Posicao;
-
 public enum ConteudoCasa {
 	MAIS("+"), XIS("x"), NADA("_");
 
 	private String conteudo;
-	private Posicao posicao;
 	
 	ConteudoCasa(final String conteudo) {
 		this.conteudo = conteudo;
@@ -17,14 +14,6 @@ public enum ConteudoCasa {
 	public String toString() {
 
 		return this.conteudo;
-	}
-
-	public Posicao getPosicao() {
-		return posicao;
-	}
-
-	public void setPosicao(Posicao posicao) {
-		this.posicao = posicao;
 	}
 }
 
